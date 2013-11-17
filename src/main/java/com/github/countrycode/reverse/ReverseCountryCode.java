8e6d155770d888c1bc5d5e56ce90fd4b2c48d8ea
Ben <ben@Satellite-Pro.(none)>
2013-11-17 19:30:12 +0000

    Added javadoc and refactor

--- src/main/java/com/github/countrycode/reverse/ReverseCountryCode.java
+++ src/main/java/com/github/countrycode/reverse/ReverseCountryCode.java
@@ -7,26 +7,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+/**
+ * Finds the two letter ISO country code (ISO 3166-1 alpha-2) for any given
+ * point in degrees latitude and longitude.
+ */
 public class ReverseCountryCode {
 
-    private final String PROPERTIES_FILE = "/polygons.properties";
+    private static final String PROPERTIES_FILE = "/polygons.properties";
 
-    private List<Geometry> geometries = new ArrayList<Geometry>();
+    private final List<Geometry> geometries = new ArrayList<Geometry>();
 
+    /**
+     * Creates a new reverse country code object. This is an expensive operation
+     * as the country boundary data is parsed each time the class instantiated.
+     */
     public ReverseCountryCode() {
+        this(ReverseCountryCode.class.getResourceAsStream(PROPERTIES_FILE));
+    }
+
+    private ReverseCountryCode(InputStream is) {
         Properties polys = new Properties();
-        InputStream is = getClass().getResourceAsStream(PROPERTIES_FILE);
         try {
             polys.load(is);
-            for (String id : polys.stringPropertyNames()) {
-                String wkt = polys.getProperty(id);
-                WktParser parser = new WktParser(new StringReader(wkt));
-                geometries.add(parser.parse(id));
-            }
-        } catch (TokenMgrError e) {
-            throw new RuntimeException(e);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
+            load(polys);
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
@@ -40,9 +43,33 @@ public class ReverseCountryCode {
         }
     }
 
-    public String getCountryCode(double x, double y) {
+    private void load(Properties polys) {
+        try {
+            for (String id : polys.stringPropertyNames()) {
+                String wkt = polys.getProperty(id);
+                WktParser parser = new WktParser(new StringReader(wkt));
+                geometries.add(parser.parse(id));
+            }
+        } catch (TokenMgrError e) {
+            throw new IllegalArgumentException(e);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Returns the ISO country code of any given point.
+     *
+     * @param lat
+     *            degrees latitude
+     * @param lon
+     *            degrees longitude
+     * @return two letter ISO country code or null if the point is not in a
+     *         country
+     */
+    public String getCountryCode(double lat, double lon) {
         for (Geometry g : geometries) {
-            if (g.contains(x, y)) {
+            if (g.contains(lat, lon)) {
                 return g.getId();
             }
         }
